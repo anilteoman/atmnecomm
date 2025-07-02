f30995032c3ce89e3a632c48e5c4445cbf2b5554
Anıl Teoman <anilteoman@outlook.com>
2025-07-02 15:00:14 +0300

    tailwindcss test

--- sekerpetshop/src/App.jsx
+++ sekerpetshop/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
       <>
-          <h1 className="text-3xl font-bold underline">
-              şeker petshop tailwind css <span className="text-5xl font-italic color-red">test</span>
+          <h1 className="text-center text-3xl font-bold">
+              şeker petshop tailwind css <span className='text-xl text-amber-200 underline'>test</span>
           </h1>
       </>
   )
