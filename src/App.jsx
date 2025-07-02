8efd7219480e75447dd52d9f514c350a0fb2237f
Anıl Teoman <anilteoman@outlook.com>
2025-07-02 15:32:54 +0300

    dependencies setup 2

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-center'>
+        <h1 className="text-3xl font-bold underline">
+    Hello world! tailwindcss test
+  </h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
