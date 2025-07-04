6e410a976ba2207fa61ce1c9eee5e4f582180375
Anıl Teoman <anilteoman@outlook.com>
2025-07-04 12:47:53 +0300

    header 1/2

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Search, ShoppingCart, Menu} from 'lucide-react';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="flex">
+      
+      <h1 className="text-3xl text-bold">ŞekerPetshop</h1>
 
-export default Header
+      <nav className="flex flex-col md:flex-row gap-2 text-3xl text-center">
+        <a href="#">Home</a>
+        <a href="#">Shop</a>
+        <a href="#">Pricing</a>
+        <a href="#">Contact</a>
+      </nav>
+       <div className="flex gap-1">
+        <a href="#"><Search/></a>
+        <a href="#"><ShoppingCart/></a>
+        <a href="#"><Menu/></a>
+      </div>
+      
+
+     
+    </header>
+  );
+};
+
+export default Header;
