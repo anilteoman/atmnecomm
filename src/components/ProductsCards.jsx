60e3ce10a1f93e82b5a4c284c88a5a829c9b7c53
Anıl Teoman <anilteoman@outlook.com>
2025-07-10 15:29:22 +0300

    adds homepage components

--- src/components/ProductsCards.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProductsCards = () => {
-  return (
-    <div>ProductsCards</div>
-  )
-}
-
-export default ProductsCards
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import BrandLogos from "../components/BrandLogos";
 import FeaturedPosts from "../components/FeaturedPosts";
 import FeaturedProducts from "../components/FeaturedProducts";
 import Hero from "../components/Hero";
-import ProductCards from "../components/ProductsCards";
+import ProductCards from "../components/ProductCards";
 import Services from "../components/Services";
 import ShopCards from "../components/ShopCards";
 
