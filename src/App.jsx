09343fcfc5b5b3ce13c2eedaf6dc6f593de448df
Anıl Teoman <anilteoman@outlook.com>
2025-07-11 12:57:43 +0300

    adds global font style

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import './App.css'
 function App() {
  
   return (
-    <>
+    <div className='font-montserrat'>
       <Header />
       <PageContent />
       <Footer />
-    </>
+    </div>
   )
 }
 
