cb704d73507ee97ae541a6f0697afea6944fed64
Anıl Teoman <anilteoman@outlook.com>
2025-07-29 12:10:01 +0300

    Update ProductDetail.jsx

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
+
+
+/*  PRODUCT DETAİL AXİOS İSTEGİ APİ DE 404 OLARAK DONUYOR . FİXLE*/
+
 const ProductDetail = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
