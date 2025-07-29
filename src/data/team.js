528b061211c556f11f04c68396f51d9a37c063e4
Anıl Teoman <anilteoman@outlook.com>
2025-07-29 12:53:35 +0300

    adds team data

--- src/data/team.js
+++ src/data/team.js
@@ -1,1 +1,31 @@
-/**team datasi yazilabilir. */
+/*team data */
+
+
+export const team = [
+  {
+    name: "Anıl Teoman",
+    role: "Full Stack Developer",
+    avatar:
+      "1538144039243.jpg",
+    id: "1",
+  },
+  {
+    name: "Emma Walker",
+    role: "Central Metrics Orchestrator",
+    avatar: "https://avatars.githubusercontent.com/u/41992156",
+    id: "2",
+  },
+  {
+    name: "Willie Ledner",
+    role: "Dynamic Response Planner",
+    avatar: "https://avatars.githubusercontent.com/u/28084390",
+    id: "3",
+  },
+  {
+    name: "Leslie Mayert",
+    role: "Legacy Solutions Coordinator",
+    avatar:
+      "https://cdn.jsdelivr.net/gh/faker-js/assets-person-portrait/female/512/4.jpg",
+    id: "4",
+  },
+];
