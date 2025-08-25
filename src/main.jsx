19db7a601cca9a6903be71b1d7053fe347f7fd24
Anıl Teoman <anilteoman@outlook.com>
2025-08-25 13:07:55 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min.js'
 import { Provider } from 'react-redux'
+import {store} from "./store/store.js"
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
